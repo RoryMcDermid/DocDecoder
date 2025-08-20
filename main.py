e0dd3da1806a58a7ae5d4a40e076b9f92158b5b3
Rory McDermid <rory.headstrong@gmail.com>
2025-08-20 16:48:03 +0100

    added line at bottom of main for example

--- main.py
+++ main.py
@@ -44,3 +44,6 @@ def runProgram(url):
     printOut(readIn(url))
     
 
+
+
+runProgram("https://docs.google.com/document/d/e/2PACX-1vRPzbNQcx5UriHSbZ-9vmsTow_R6RRe7eyAU60xIF9Dlz-vaHiHNO2TKgDi7jy4ZpTpNqM7EvEcfr_p/pub")
